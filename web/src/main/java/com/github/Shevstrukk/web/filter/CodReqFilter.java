2e88aad80fc2aaa2ee47bd69c948e1c84edb9946
Vitalij <Shevstrukk@mail.ru>
2019-10-09 20:55:24 +0300

    added CodReqFilter

---
+++ web/src/main/java/com/github/Shevstrukk/web/filter/CodReqFilter.java
@@ -0,0 +1,27 @@
+package com.github.Shevstrukk.web.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+  @WebFilter("/update")
+public class CodReqFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        servletRequest.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
