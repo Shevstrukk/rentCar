32904a9c9d70c0bd5b944a5d111d57b65aa06778
Vitalij <Shevstrukk@mail.ru>
2019-10-09 20:53:37 +0300

    added AddPersonFilter

---
+++ web/src/main/java/com/github/Shevstrukk/web/filter/AddPersonFilter.java
@@ -0,0 +1,18 @@
+package com.github.Shevstrukk.web.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/addPerson")
+public class AddPersonFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+}
+
