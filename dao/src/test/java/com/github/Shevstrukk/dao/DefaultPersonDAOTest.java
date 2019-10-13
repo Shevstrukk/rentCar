ff0856e19d94b46f21749f433352742c9ed7e426
Vitalij <Shevstrukk@mail.ru>
2019-10-13 17:45:46 +0300

    add methods into DefaultPersonDAOTest : update, insert, getPerson, delete

--- dao/src/test/java/com/github/Shevstrukk/dao/DefaultPersonDAOTest.java
+++ dao/src/test/java/com/github/Shevstrukk/dao/DefaultPersonDAOTest.java
@@ -33,17 +33,36 @@ public class DefaultPersonDAOTest {
         assertTrue(deleted);
         final Person afterDelete = personDAO.getPerson(id);
         assertNull(afterDelete);
-      /*
-       final SalaryDto salaryToSave = new SalaryDto(null, 30, "dev");
-        final SalaryDto savedSalary = salaryDao.save(salaryToSave);
-        final Long id = savedSalary.getId();
-       final SalaryDto salaryDto = salaryDao.get(id);
-        assertNotNull(salaryDto);
-        final boolean deleted = salaryDao.delete(id);
-        assertTrue(deleted);
 
-        final SalaryDto afterDelete = salaryDao.get(id);
-        assertNull(afterDelete);
-*/
+    }
+
+    @Test
+    public void getPerson() {
+        final Person person = new Person(null, "dddd", "hhhh", 6);
+        final Person savePerson = personDAO.insertPerson(person);
+        final Long id = savePerson.getId();
+        final Person person1 = personDAO.getPerson(id);
+        assertNotNull(person1);
+        assertEquals(person.getFirstName(), person1.getFirstName());
+        assertEquals(person.getLastName(), person1.getLastName());
+        assertEquals(person.getRentDay(), person1.getRentDay());
+        assertEquals(id, person1.getId());
+
+
+    }
+
+    @Test
+    public void updatePerson() {
+        final Person person = new Person(null, "TT", "SS", 8);
+        final Person savePerson = personDAO.insertPerson(person);
+        final Long id = savePerson.getId();
+        final Person updatePerson = new Person(id, savePerson.getFirstName(), savePerson.getLastName(), 77);
+        final boolean isUpdate = personDAO.updatePerson(updatePerson);
+        assertTrue(isUpdate);
+        final Person afterPerson = personDAO.getPerson(id);
+        assertEquals(updatePerson.getFirstName(), afterPerson.getFirstName());
+        assertEquals(updatePerson.getLastName(), afterPerson.getLastName());
+        assertEquals(updatePerson.getRentDay(), afterPerson.getRentDay());
+
     }
 }
