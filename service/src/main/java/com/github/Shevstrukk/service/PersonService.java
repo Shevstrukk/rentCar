cd6525b5130d68c7d3062f3eb4eaa12e12859f8c
Vitalij <Shevstrukk@mail.ru>
2019-10-13 16:00:12 +0300

    change delete(Person person) on delete(long id)

--- service/src/main/java/com/github/Shevstrukk/service/PersonService.java
+++ service/src/main/java/com/github/Shevstrukk/service/PersonService.java
@@ -3,13 +3,12 @@ package com.github.Shevstrukk.service;
 import com.github.Shevstrukk.model.Person;
 
 
-import java.sql.SQLException;
 import java.util.List;
 
 public interface PersonService {
     public void insertPerson(Person person);
     public List<Person> listAllPerson();
-    public void deletePerson(Person person) ;
+    public void deletePerson(int person) ;
     public void updatePerson(Person person) ;
     public Person getPerson(int id) ;
 }
