87ed758a338f7c177f7320aadb89c6ac69aeeb79
Vitalij <Shevstrukk@mail.ru>
2019-10-23 23:26:04 +0300

    add dependece, user

--- dao/src/main/java/com/github/Shevstrukk/dao/entity/User.java
+++ dao/src/main/java/com/github/Shevstrukk/dao/entity/User.java
@@ -1,4 +1,64 @@
 package com.github.Shevstrukk.dao.entity;
 
+@Entity
+@Table
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String login;
+
+    @Column
+    private String password;
+
+    @Column
+    private String role;
+
+    public User() {
+    }
+
+    public User(Long id, String login, String password, String role) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
 }
+
