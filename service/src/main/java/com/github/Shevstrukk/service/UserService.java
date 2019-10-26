9f174f014357a57a58558d3f72654c8b3cee0c23
Vitalij <Shevstrukk@mail.ru>
2019-10-26 16:20:14 +0300

    add addAuthUser

--- service/src/main/java/com/github/Shevstrukk/service/UserService.java
+++ service/src/main/java/com/github/Shevstrukk/service/UserService.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface UserService {
     public List<AuthUser> listAllUsers();
      public AuthUser login(String login, String password);
+    public AuthUser addAuthUser(String login, String password);
 }
