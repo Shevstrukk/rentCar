8dd763e52664da00b95c5c4c314f471ffc90ae31
Vitalij <Shevstrukk@mail.ru>
2019-10-26 13:28:04 +0300

    changeEMUtil

--- dao/src/main/java/com/github/Shevstrukk/dao/util/EMUtil.java
+++ dao/src/main/java/com/github/Shevstrukk/dao/util/EMUtil.java
@@ -1,24 +1,28 @@
 package com.github.Shevstrukk.dao.util;
 
+import org.hibernate.Session;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class EMUtil {
-    private static EntityManagerFactory emFactory = null;
+    //
+    private static final EntityManagerFactory emFactory;
 
-    public static EntityManager getEntityManager() {
-        return getEntityManager("com.github.Shevstrukk.dao");
+    static {
+        emFactory = Persistence.createEntityManagerFactory("com.github.Shevstrukk.dao");
     }
 
-    public static EntityManager getEntityManager(String unit) {
-        if (emFactory == null) {
-            emFactory = Persistence.createEntityManagerFactory(unit);
-        }
+    public static EntityManager getEntityManager() {
         return emFactory.createEntityManager();
     }
 
-    public static void closeEMFactory() {
+    public static Session getSession() {
+        return getEntityManager().unwrap(Session.class);
+    }
+
+    public static void close() {
         emFactory.close();
     }
 }
