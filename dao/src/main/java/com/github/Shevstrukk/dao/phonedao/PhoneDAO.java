a42003048790a582d758fc05bac87e53eaf62497
Vitalij <Shevstrukk@mail.ru>
2019-11-03 11:43:04 +0300

    add return List<Phone>

--- dao/src/main/java/com/github/Shevstrukk/dao/phonedao/PhoneDAO.java
+++ dao/src/main/java/com/github/Shevstrukk/dao/phonedao/PhoneDAO.java
@@ -1,7 +1,10 @@
 package com.github.Shevstrukk.dao.phonedao;
 
+import com.github.Shevstrukk.dao.entity.Person;
 import com.github.Shevstrukk.dao.entity.Phone;
 
+import java.util.List;
+
 public interface PhoneDAO {
-    public void insertPhone(Phone phone);
+    public List<Phone> savePhone(Phone phone, int id);
 }
