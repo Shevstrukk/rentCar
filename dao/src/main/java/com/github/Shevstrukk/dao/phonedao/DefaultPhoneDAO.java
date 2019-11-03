f6c399805bc3ac9a57e0539ab198a4e6cf027afb
Vitalij <Shevstrukk@mail.ru>
2019-11-03 11:42:27 +0300

    add return List<Phone>

--- dao/src/main/java/com/github/Shevstrukk/dao/phonedao/DefaultPhoneDAO.java
+++ dao/src/main/java/com/github/Shevstrukk/dao/phonedao/DefaultPhoneDAO.java
@@ -1,10 +1,14 @@
 package com.github.Shevstrukk.dao.phonedao;
 
+import com.github.Shevstrukk.dao.entity.Person;
 import com.github.Shevstrukk.dao.entity.Phone;
 import com.github.Shevstrukk.dao.util.EMUtil;
 import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.persistence.Query;
+import java.util.List;
 
 public class DefaultPhoneDAO implements PhoneDAO {
     private static final Logger log = LoggerFactory.getLogger(DefaultPhoneDAO.class);
@@ -17,11 +21,21 @@ public class DefaultPhoneDAO implements PhoneDAO {
     public static PhoneDAO getInstance() {
         return SingletonHolder.HOLDER_INSTANCE;
     }
-    public void insertPhone(Phone phone){
+
+    public List<Phone> savePhone(Phone phone, int id){
+        List<Phone> phones;
         Session session = EMUtil.getSession();
         session.beginTransaction();
-        session.save(phone);
+        Person person = session.get(Person.class, id);
+       // person.getPhones().clear();
+        person.getPhones().add(phone);
+        phone.setPerson(person);
+        session.saveOrUpdate(person);
+        phones = person.getPhones();
+      //  phone.setPerson(person);
+        //session.save(phone);
         session.getTransaction().commit();
         session.close();
+        return phones;
     }
 }
