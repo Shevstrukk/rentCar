da173c345aa7b7975ea6f23e426b059632fe9335
Vitalij <Shevstrukk@mail.ru>
2019-11-03 11:47:47 +0300

    add return List<Phone>

--- service/src/main/java/com/github/Shevstrukk/service/phoneservice/PhoneService.java
+++ service/src/main/java/com/github/Shevstrukk/service/phoneservice/PhoneService.java
@@ -1,7 +1,10 @@
 package com.github.Shevstrukk.service.phoneservice;
 
+import com.github.Shevstrukk.dao.entity.Person;
 import com.github.Shevstrukk.dao.entity.Phone;
 
+import java.util.List;
+
 public interface PhoneService {
-    public void savePhone(Phone phone);
+    public List<Phone> savePhone(Phone phone, int id);
 }
