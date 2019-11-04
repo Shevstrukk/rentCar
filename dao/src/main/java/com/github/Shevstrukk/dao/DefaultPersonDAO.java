33241bf092e8a0b004aab420f75d567a39b78ff2
Vitalij <Shevstrukk@mail.ru>
2019-11-04 13:43:27 +0300

    add listAiiAuthUsers

--- dao/src/main/java/com/github/Shevstrukk/dao/DefaultPersonDAO.java
+++ dao/src/main/java/com/github/Shevstrukk/dao/DefaultPersonDAO.java
@@ -1,6 +1,11 @@
 package com.github.Shevstrukk.dao;
 
-import com.github.Shevstrukk.model.Person;
+import com.github.Shevstrukk.dao.entity.Address;
+import com.github.Shevstrukk.dao.entity.AuthUser;
+import com.github.Shevstrukk.dao.entity.Person;
+import com.github.Shevstrukk.dao.util.EMUtil;
+
+import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,18 +16,7 @@ import java.util.ResourceBundle;
 
 public class DefaultPersonDAO implements PersonDAO {
     private static final Logger log = LoggerFactory.getLogger(DefaultPersonDAO.class);
-
-    String jdbcURL;
-    String jdbcUsername;
-    String jdbcPassword;
-
     public DefaultPersonDAO() {
-
-        ResourceBundle resources = ResourceBundle.getBundle("db");
-        jdbcURL = resources.getString("url");
-        jdbcUsername = resources.getString("user");
-        jdbcPassword = resources.getString("password");
-
     }
 
     private static class SingletonHolder {
@@ -33,151 +27,64 @@ public class DefaultPersonDAO implements PersonDAO {
         return SingletonHolder.HOLDER_INSTANCE;
     }
 
-    Connection jdbcConnection;
-
-    protected void connect() throws SQLException {
-
-        if (jdbcConnection == null || jdbcConnection.isClosed()) {
-
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-            jdbcConnection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
-        }
-    }
-
-    protected void disconnect() throws SQLException {
-        if (jdbcConnection != null && !jdbcConnection.isClosed()) {
-            jdbcConnection.close();
-        }
-    }
-
     public Person insertPerson(Person person) {
-        String sql = "INSERT INTO person ( first_name, last_name, rent_day) VALUES (?, ?, ?)";
-        try {
-            connect();
-            PreparedStatement statement = jdbcConnection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, person.getFirstName());
-            statement.setString(2, person.getLastName());
-            statement.setFloat(3, person.getRentDay());
-            //boolean rowInserted =
-            statement.executeUpdate();
-            //  return rowInserted;
-            final ResultSet generatedKeys = statement.getGeneratedKeys();
-            generatedKeys.next();
-            final long id = generatedKeys.getInt(1);
-            final Person personInsert = new Person(id, person.getFirstName(), person.getLastName(), person.getRentDay());
-            log.info("person saved: {}", personInsert);
-            statement.close();
-            disconnect();
-            return personInsert;
-        } catch (SQLException e) {
-            log.error("fail to insert person salary:{}", person, e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    public List<Person> listAllPerson() {
-        List<Person> listPerson = new ArrayList<>();
-        String sql = "SELECT * FROM person";
-        try {
-            connect();
-            Statement statement = jdbcConnection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            while (resultSet.next()) {
-                long id = resultSet.getInt("person_id");
-                String firstName = resultSet.getString("first_name");
-                String lastName = resultSet.getString("last_name");
-                int rentDay = resultSet.getInt("rent_day");
-
-                Person person = new Person(id, firstName, lastName, rentDay);
-                listPerson.add(person);
-                log.info("person saved: {}", person);
-            }
-            resultSet.close();
-            statement.close();
-
-            disconnect();
-        } catch (SQLException e) {
-            log.error("fail return Listperson:{}", listPerson, e);
-            throw new RuntimeException();
-        }
-        return listPerson;
-    }
-
-    public boolean deletePerson(long person)  {
-
-
-        String sql = "DELETE FROM person where person_id = ?";
-        boolean rowDeleted=false;
-        try {
-            connect();
-        PreparedStatement statement = jdbcConnection.prepareStatement(sql);
-       // statement.setInt(1, person.getId());
-            statement.setLong(1, person);
-
-        rowDeleted = statement.executeUpdate() > 0;
-        statement.close();
-        disconnect();
-
-      } catch (SQLException e) {
-            log.error("fail delete person:{}", rowDeleted, e);
-            throw new RuntimeException(e);
-      }
-        return rowDeleted;
-    }
-
-    public boolean updatePerson(Person person)  {
-        String sql = "UPDATE person SET first_name = ?, last_name = ?, rent_day = ?";
-        sql += " WHERE person_id = ?";
-        boolean rowUpdated=false;
-        try {
-            connect();
-            PreparedStatement statement = jdbcConnection.prepareStatement(sql);
-
-              //  connect();
-                statement.setString(1, person.getFirstName());
-                statement.setString(2, person.getLastName());
-                statement.setInt(3, person.getRentDay());
-                statement.setLong(4, person.getId());
-                rowUpdated = statement.executeUpdate() > 0;
-                statement.close();
-                disconnect();
-                return rowUpdated;
-            } catch (SQLException e) {
-                log.error("fail update person:{}", rowUpdated, e);
-                throw new RuntimeException(e);
-            }
-
-
-    }
-
-    public Person getPerson(long id)  {
-        Person person = null;
-        String sql = "SELECT * FROM person WHERE person_id = ?";
-        try {
-        connect();
-        PreparedStatement statement = jdbcConnection.prepareStatement(sql);
-        statement.setLong(1, id);
-
-        ResultSet resultSet = statement.executeQuery();
-
-        if (resultSet.next()) {
-            final long resultId = resultSet.getInt("person_id");
-            String firstName = resultSet.getString("first_name");
-            String lastName = resultSet.getString("last_name");
-            int rentDay = resultSet.getInt("rent_day");
-            person = new Person(resultId, firstName, lastName, rentDay);
-        }
-        resultSet.close();
-        statement.close();}
-        catch (SQLException e){
-            log.error("fail update person:{}", person, e);
-            throw new RuntimeException(e);
-        }
+        Session session = EMUtil.getSession();
+        session.beginTransaction();
+        AuthUser authUser = person.getAuthUser();
+        Address address = person.getAddress();
+        session.save(address);
+        authUser.setPerson(person);
+        person.setAddress(address);
+        session.save(person);
+        session.saveOrUpdate(authUser);
+        session.getTransaction().commit();
+        session.close();
         return person;
+    }
+    public Person getPerson(int id) {
+        Person person = null;
+        Session session = EMUtil.getSession();
+        session.beginTransaction();
+        person = session.get(Person.class, id);
+        session.getTransaction().commit();
+        session.close();
+        return person;
+        //String sql = "SELECT * FROM person WHERE person_id = ?";
+    }
+
+    @Override
+    public List<AuthUser> listAllAuthUsers() {
+        List<AuthUser> list;
+        Session session = EMUtil.getSession();
+        session.beginTransaction();
+        String str = "FROM AuthUser  ORDER BY id ASC";
+        list = session.createQuery(str).getResultList();
+        session.getTransaction().commit();
+        session.close();
+        return list;
+    }
+    public List<Person> listAllPerson() {
+
+        List<Person> list;
+        Session session = EMUtil.getSession();
+        session.beginTransaction();
+       // String str = "FROM Person  ORDER BY id ASC";
+        String str = "FROM  Person e JOIN FETCH e.phones phon";
+        list = session.createQuery(str).getResultList();
+        session.getTransaction().commit();
+        session.close();
+        return list;
+    }
+    public void updatePerson(Person person){
+        int id = person.getId();
+        Session session = EMUtil.getSession();
+        session.beginTransaction();
+        Person person1 = session.get(Person.class,id);
+        person1.setFirstName(person.getFirstName());
+        person1.setLastName(person.getLastName());
+        person1.setAddress(person.getAddress());
+        session.saveOrUpdate(person1);
+        session.getTransaction().commit();
+        session.close();
     }
 }
