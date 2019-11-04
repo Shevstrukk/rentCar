ddd476b69252dfc1af0bc806c3c8f1b245770844
Vitalij <Shevstrukk@mail.ru>
2019-11-04 14:26:13 +0300

    add imports

--- service/src/main/java/com/github/Shevstrukk/service/PersonService.java
+++ service/src/main/java/com/github/Shevstrukk/service/PersonService.java
@@ -1,14 +1,17 @@
 package com.github.Shevstrukk.service;
 
-import com.github.Shevstrukk.model.Person;
 
+
+import com.github.Shevstrukk.dao.entity.AuthUser;
+import com.github.Shevstrukk.dao.entity.Person;
 
 import java.util.List;
 
 public interface PersonService {
     public Person insertPerson(Person person);
+   public List<AuthUser> listAllAuthUser();
     public List<Person> listAllPerson();
-    public void deletePerson(int person) ;
+  // public void deletePerson(int person) ;
     public void updatePerson(Person person) ;
     public Person getPerson(int id) ;
 }
