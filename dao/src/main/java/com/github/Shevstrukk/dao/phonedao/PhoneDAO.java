0a0ff36c1f9eed4f6175646705f3f120e7901622
Vitalij <Shevstrukk@mail.ru>
2019-11-04 13:57:45 +0300

    add deletePhone

--- dao/src/main/java/com/github/Shevstrukk/dao/phonedao/PhoneDAO.java
+++ dao/src/main/java/com/github/Shevstrukk/dao/phonedao/PhoneDAO.java
@@ -6,5 +6,6 @@ import com.github.Shevstrukk.dao.entity.Phone;
 import java.util.List;
 
 public interface PhoneDAO {
-    public List<Phone> savePhone(Phone phone, int id);
+    public Person savePhone(Phone phone, int id);
+    public Person deletePhone( int personId, int id);
 }
