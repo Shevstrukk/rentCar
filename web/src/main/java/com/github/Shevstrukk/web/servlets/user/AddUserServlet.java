8ffdc4092e9790cb5f9af14d982b6602b255bc2a
Vitalij <Shevstrukk@mail.ru>
2019-11-04 13:40:03 +0300

    delete imports

--- web/src/main/java/com/github/Shevstrukk/web/servlets/user/AddUserServlet.java
+++ web/src/main/java/com/github/Shevstrukk/web/servlets/user/AddUserServlet.java
@@ -1,6 +1,9 @@
 package com.github.Shevstrukk.web.servlets.user;
 
-import com.github.Shevstrukk.model.Person;
+
+import com.github.Shevstrukk.dao.entity.Address;
+import com.github.Shevstrukk.dao.entity.AuthUser;
+import com.github.Shevstrukk.dao.entity.Person;
 import com.github.Shevstrukk.service.DefaultPersonService;
 import com.github.Shevstrukk.service.PersonService;
 
@@ -11,8 +14,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
 
 @WebServlet("/addUser")
 public class AddUserServlet extends HttpServlet {
@@ -20,17 +21,28 @@ public class AddUserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final String firstName = req.getParameter("firstName");
         final String lastName = req.getParameter("lastName");
-        final int rentDay = Integer.valueOf(req.getParameter("rentDay"));
-        Person person = new Person(null,firstName,lastName,rentDay);
-            DefaultPersonService.getInstance().insertPerson(person);
-        doGet(req,resp);
+        final String state = req.getParameter("state");
+        final String city = req.getParameter("city");
+        final String street = req.getParameter("street");
+        final int home = Integer.valueOf(req.getParameter("home"));
+        final int number = Integer.valueOf(req.getParameter("number"));
+        Address address = new Address(null,state, city, street, home,number);
+        AuthUser authUser = (AuthUser)req.getSession().getAttribute("authUser");
+        Person person = new Person(null,firstName,lastName,authUser,address,null);
+       // req.setAttribute("personid", person.getId());
+       Person person1= DefaultPersonService.getInstance().insertPerson(person);
+        req.getSession().setAttribute("person1", person1);
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/phone/addPhone.jsp");
+        requestDispatcher.forward(req, resp);
+       // DefaultPersonService.getInstance().insertPerson(person);
+       // doGet(req,resp);
     }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PersonService defaultPersonService= DefaultPersonService.getInstance();
+       /* PersonService defaultPersonService= DefaultPersonService.getInstance();
         List<Person> personList = defaultPersonService.listAllPerson();
         req.setAttribute("personList", personList);
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/user/userList.jsp");
-        requestDispatcher.forward(req, resp);
+        requestDispatcher.forward(req, resp);*/
     }
 }
