70eba4f4bdca7fd9a74de73feee496eb069cf4f0
Vitalij <Shevstrukk@mail.ru>
2019-11-04 13:53:11 +0300

    change doGet

--- web/src/main/java/com/github/Shevstrukk/web/servlets/DeletePersonServlet.java
+++ web/src/main/java/com/github/Shevstrukk/web/servlets/DeletePersonServlet.java
@@ -1,9 +1,12 @@
 package com.github.Shevstrukk.web.servlets;
 
-import com.github.Shevstrukk.model.Person;
-import com.github.Shevstrukk.model.User;
+
+import com.github.Shevstrukk.dao.entity.Person;
 import com.github.Shevstrukk.service.DefaultPersonService;
+import com.github.Shevstrukk.service.DefaultUserService;
+
 import com.github.Shevstrukk.service.PersonService;
+import com.github.Shevstrukk.service.UserService;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,17 +15,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.List;
+
 
 @WebServlet("/delete")
 public class DeletePersonServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PersonService defaultPersonService= DefaultPersonService.getInstance();
+        //UserService defaultUserService = DefaultUserService.getInstance();
+       // List<AuthUser> authUserList = defaultUserService.listAllUsers();
+        PersonService defaultPersonService = DefaultPersonService.getInstance();
         List<Person> personList = defaultPersonService.listAllPerson();
         req.setAttribute("personList", personList);
-        User user = (User) req.getSession().getAttribute("user");
+      //  User user = (User) req.getSession().getAttribute("user");
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/personList.jsp");
         requestDispatcher.forward(req, resp);
     }
@@ -30,9 +35,9 @@ public class DeletePersonServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-  int id = Integer.parseInt(req.getParameter("id"));
+            Integer id = Integer.parseInt(req.getParameter("id"));
       //  Person person = new Person(id);
-            DefaultPersonService.getInstance().deletePerson(id);
-doGet(req,resp);
+            DefaultUserService.getInstance().deleteAuthUser(id);
+            doGet(req,resp);
     }
-}
+    }
