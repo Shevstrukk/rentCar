36f4482bab5e2e3c3d6328715f2ed3daf223c2e3
Vitalij <Shevstrukk@mail.ru>
2019-11-04 13:36:49 +0300

    delete imports

--- web/src/main/java/com/github/Shevstrukk/web/servlets/phone/AddPhoneServlet.java
+++ web/src/main/java/com/github/Shevstrukk/web/servlets/phone/AddPhoneServlet.java
@@ -1,6 +1,7 @@
 package com.github.Shevstrukk.web.servlets.phone;
 
 import com.github.Shevstrukk.dao.entity.AuthUser;
+import com.github.Shevstrukk.dao.entity.Person;
 import com.github.Shevstrukk.dao.entity.Phone;
 import com.github.Shevstrukk.service.phoneservice.DefaultPhoneService;
 
@@ -10,21 +11,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/addPhone")
 public class AddPhoneServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final String line = String.valueOf(req.getParameter("phone"));
-        AuthUser user = (AuthUser) req.getSession().getAttribute("authUser");
-        Integer userId = user.getId();
-        Phone phone = new Phone(null,line,null, userId);
-        DefaultPhoneService.getInstance().savePhone(phone);
+        Person person1 = (Person) req.getSession().getAttribute("person1");
+         int id = person1.getId();
+        Phone phone = new Phone(null,line,null);
+        //Integer userId = user.getId();
+        DefaultPhoneService.getInstance().savePhone(phone, id);
 
     }
 }
