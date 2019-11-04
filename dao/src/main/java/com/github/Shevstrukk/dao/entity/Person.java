6ebccb9592e31e78297714dc87ad8df90257ce9a
Vitalij <Shevstrukk@mail.ru>
2019-11-04 14:23:10 +0300

    change annotation

--- dao/src/main/java/com/github/Shevstrukk/dao/entity/Person.java
+++ dao/src/main/java/com/github/Shevstrukk/dao/entity/Person.java
@@ -1,27 +1,44 @@
 package com.github.Shevstrukk.dao.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "person")
 public class Person {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private  Integer id;
+
+    @Column(name = "name")
     private  String firstName;
+
+    @Column(name = "surname")
     private  String lastName;
+
+    @OneToOne(mappedBy = "person", cascade = CascadeType.ALL)
     private AuthUser authUser;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="address_id")
+    private Address address;
+
+    @OneToMany(mappedBy = "person", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Phone> phones = new ArrayList<>();
 
     public Person() {}
 
-    public Person(Integer id, String firstName, String lastName, AuthUser authUser) {
+    public Person(Integer id, String firstName, String lastName,
+                  AuthUser authUser, Address address, List<Phone> phone) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.authUser = authUser;
+        this.address = address;
+        this.phones=phone;
     }
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+
     public Integer getId() {
         return id;
     }
@@ -29,30 +46,38 @@ public class Person {
     public void setId(Integer id) {
         this.id = id;
     }
-    @Column(name = "name")
+
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-    @Column(name = "surname")
+
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    @OneToOne(mappedBy = "person")
-   // @JoinColumn(name = "person_id")
-    public AuthUser getAuthUser() {
+
+       public AuthUser getAuthUser() {
         return authUser;
     }
-
     public void setAuthUser(AuthUser authUser) {
         this.authUser = authUser;
+    }
+
+    public Address getAddress() {   return address;    }
+    public void setAddress(Address address) {     this.address = address;    }
+
+    public List<Phone> getPhones() {        return phones;    }
+
+    public void setPhones(List<Phone> phones) {        this.phones = phones;    }
+
+    public void addPhones(Phone phone){
+        phone.setPerson(this);
+        phones.add(phone);
     }
 }
