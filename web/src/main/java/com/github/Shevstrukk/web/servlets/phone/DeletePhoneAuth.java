603daad94cc58ef1f66161e0b5deeac6721389e8
Vitalij <Shevstrukk@mail.ru>
2019-11-04 13:49:45 +0300

    addDeletePhoneAuth

---
+++ web/src/main/java/com/github/Shevstrukk/web/servlets/phone/DeletePhoneAuth.java
@@ -0,0 +1,27 @@
+package com.github.Shevstrukk.web.servlets.phone;
+
+import com.github.Shevstrukk.dao.entity.Person;
+import com.github.Shevstrukk.service.phoneservice.DefaultPhoneService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/deletePhone")
+public class DeletePhoneAuth extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        int personId = Integer.parseInt(req.getParameter("personId"));
+
+       Person person =  DefaultPhoneService.getInstance().deletePhone( personId, id);
+        doGet(req,resp);
+    }
+}
