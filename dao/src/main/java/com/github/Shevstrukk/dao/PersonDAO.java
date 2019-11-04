331987159d63a73333d216319caaa83d72abbb27
Vitalij <Shevstrukk@mail.ru>
2019-11-04 14:24:37 +0300

    add listAllAuthUser

--- dao/src/main/java/com/github/Shevstrukk/dao/PersonDAO.java
+++ dao/src/main/java/com/github/Shevstrukk/dao/PersonDAO.java
@@ -1,14 +1,18 @@
 package com.github.Shevstrukk.dao;
 
-import com.github.Shevstrukk.model.Person;
+
+
+import com.github.Shevstrukk.dao.entity.AuthUser;
+import com.github.Shevstrukk.dao.entity.Person;
 
 import java.util.List;
 
 
 public interface PersonDAO {
     public Person insertPerson(Person person);
+   public List<AuthUser> listAllAuthUsers();
     public List<Person> listAllPerson();
-    public boolean deletePerson(long person) ;
-    public boolean updatePerson(Person person) ;
-    public Person getPerson(long id) ;
+  /*  public boolean deletePerson(long person) ;*/
+    public void updatePerson(Person person) ;
+    public Person getPerson(int id) ;
 }
