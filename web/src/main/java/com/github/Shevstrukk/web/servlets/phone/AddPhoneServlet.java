8a0fe601f8fde3f630a16653eba8b39e40d62069
Vitalij <Shevstrukk@mail.ru>
2019-11-06 19:04:02 +0300

    add Person in session

--- web/src/main/java/com/github/Shevstrukk/web/servlets/phone/AddPhoneServlet.java
+++ web/src/main/java/com/github/Shevstrukk/web/servlets/phone/AddPhoneServlet.java
@@ -5,6 +5,7 @@ import com.github.Shevstrukk.dao.entity.Person;
 import com.github.Shevstrukk.dao.entity.Phone;
 import com.github.Shevstrukk.service.phoneservice.DefaultPhoneService;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,16 +19,23 @@ import java.util.List;
 public class AddPhoneServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Person person = (Person) req.getSession().getAttribute("person");
+        List<Phone> listPhone = person.getPhones();
+        req.getSession().setAttribute("phoneList", listPhone);
+        req.getSession().setAttribute("person", person);
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/phone/addPhone.jsp");
+        requestDispatcher.forward(req, resp);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final String line = String.valueOf(req.getParameter("phone"));
         Person person1 = (Person) req.getSession().getAttribute("person1");
-         int id = person1.getId();
+        int id = person1.getId();
         Phone phone = new Phone(null,line,null);
-        //Integer userId = user.getId();
-        DefaultPhoneService.getInstance().savePhone(phone, id);
-
+        Person person = DefaultPhoneService.getInstance().savePhone(phone, id);
+        req.getSession().setAttribute("person", person);
+        doGet(req, resp);
     }
 }
