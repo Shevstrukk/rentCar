d166be586ee895d18439671abac15483c05dfb6c
Vitalij <Shevstrukk@mail.ru>
2019-11-06 22:44:10 +0300

    add method updatePerson

--- service/src/main/java/com/github/Shevstrukk/service/PersonService.java
+++ service/src/main/java/com/github/Shevstrukk/service/PersonService.java
@@ -3,6 +3,7 @@ package com.github.Shevstrukk.service;
 
 
 import com.github.Shevstrukk.dao.entity.AuthUser;
+import com.github.Shevstrukk.dao.entity.Order;
 import com.github.Shevstrukk.dao.entity.Person;
 
 import java.util.List;
@@ -13,5 +14,6 @@ public interface PersonService {
     public List<Person> listAllPerson();
   // public void deletePerson(int person) ;
     public void updatePerson(Person person) ;
+    public Person updatePerson(Person person, Order order) ;
     public Person getPerson(int id) ;
 }
