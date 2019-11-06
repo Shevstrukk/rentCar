762e8a911c37306e4de1d98289b47d0bb9f8f5f2
Vitalij <Shevstrukk@mail.ru>
2019-11-06 18:59:57 +0300

    add DefaultOrderService

---
+++ service/src/main/java/com/github/Shevstrukk/service/orderService/DefaultOrderService.java
@@ -0,0 +1,30 @@
+package com.github.Shevstrukk.service.orderService;
+
+import com.github.Shevstrukk.dao.entity.Car;
+import com.github.Shevstrukk.dao.entity.Order;
+import com.github.Shevstrukk.dao.orderDao.DefaultOrderDAO;
+import com.github.Shevstrukk.dao.orderDao.OrderDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DefaultOrderService implements OrderService {
+    private static final Logger log = LoggerFactory.getLogger(DefaultOrderService.class);
+
+    private OrderDAO orderDAO = DefaultOrderDAO.getInstance();
+
+    private OrderService orderService = DefaultOrderService.getInstance();
+
+    public DefaultOrderService() {}
+
+    private static class SingletonHolder {
+        static final OrderService HOLDER_INSTANCE = new DefaultOrderService();
+    }
+
+    public static OrderService getInstance() {
+        return SingletonHolder.HOLDER_INSTANCE;
+    }
+
+    public Order saveOrder(Order order, Car car) { return orderDAO.saveOrder(order, car);
+
+    }
+}
