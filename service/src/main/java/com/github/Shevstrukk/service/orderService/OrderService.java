da6cbeba1b751eec9d4689aa1b4003699fa2bf2e
Vitalij <Shevstrukk@mail.ru>
2019-11-08 09:38:51 +0300

    add method getOrderList

--- service/src/main/java/com/github/Shevstrukk/service/orderService/OrderService.java
+++ service/src/main/java/com/github/Shevstrukk/service/orderService/OrderService.java
@@ -2,8 +2,12 @@ package com.github.Shevstrukk.service.orderService;
 
 import com.github.Shevstrukk.dao.entity.Car;
 import com.github.Shevstrukk.dao.entity.Order;
+import com.github.Shevstrukk.dao.entity.Person;
+
+import java.util.List;
 
 public interface OrderService {
     public Order saveOrder(Order order, Car car);
     public Order saveOrUpdate(Order order, Car car);
+    public Person getOrderList(int id);
 }
