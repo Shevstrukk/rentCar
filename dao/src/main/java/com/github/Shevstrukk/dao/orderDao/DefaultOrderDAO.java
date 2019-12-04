20d6cb7f6b53f34304ece4603260075a457b6d48
Vitalij <Shevstrukk@mail.ru>
2019-12-04 23:33:01 +0300

    add SF

--- dao/src/main/java/com/github/Shevstrukk/dao/orderDao/DefaultOrderDAO.java
+++ dao/src/main/java/com/github/Shevstrukk/dao/orderDao/DefaultOrderDAO.java
@@ -11,57 +11,59 @@ import com.github.Shevstrukk.model.Car;
 import com.github.Shevstrukk.model.Order;
 import com.github.Shevstrukk.model.Person;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public class DefaultOrderDAO implements OrderDAO {
     private static final Logger log = LoggerFactory.getLogger(DefaultOrderDAO.class);
-    public DefaultOrderDAO() {    }
+    private final SessionFactory sessionFactory;
+    public DefaultOrderDAO(SessionFactory sessionFactory) {this.sessionFactory = sessionFactory;    }
 
     @Override
     public Order saveOrder(Order order, int id) {
         OrderEntity orderEntity = OrderConverter.toEntity(order);
-        Session session = EMUtil.getSession();
-        session.beginTransaction();
+        Session session = sessionFactory.getCurrentSession();//EMUtil.getSession();
+//        session.beginTransaction();
         CarEntity carEntity1  = session.get(CarEntity.class, id);
         orderEntity.getCarEntities().add(carEntity1);
         carEntity1.getOrderEntities().add(orderEntity);
         session.saveOrUpdate(orderEntity);
-        session.getTransaction().commit();
-        session.clear();
-        session.close();
+//        session.getTransaction().commit();
+//        session.clear();
+//        session.close();
         return OrderConverter.fromEntity(orderEntity);
     }
     public Order saveUpdate(Order order, int id){
-        Session session = EMUtil.getSession();
-        session.beginTransaction();
+        Session session = sessionFactory.getCurrentSession();//EMUtil.getSession();
+//        session.beginTransaction();
         CarEntity carEntity = session.get(CarEntity.class, id);
         OrderEntity orderEntity1 = session.get(OrderEntity.class, order.getId());
         orderEntity1.setPrice(order.getPrice());
         orderEntity1.getCarEntities().add(carEntity);
         carEntity.getOrderEntities().add(orderEntity1);
         session.saveOrUpdate(orderEntity1);
-        session.getTransaction().commit();
-        session.close();
+//        session.getTransaction().commit();
+//        session.close();
         return OrderConverter.fromEntity(orderEntity1);
     }
     public Person getOrderList(int id){
-        Session session = EMUtil.getSession();
-        session.beginTransaction();
+        Session session = sessionFactory.getCurrentSession();//EMUtil.getSession();
+//        session.beginTransaction();
         String str = "FROM  PersonEntity e JOIN FETCH e.orderEntities ordered WHERE e.id=:id ";
         Query query = session.createQuery(str);
         query.setParameter("id", id);
         PersonEntity person = (PersonEntity) query.uniqueResult();
-        session.getTransaction().commit();
-        session.close();
+//        session.getTransaction().commit();
+//        session.close();
         return PersonConverter.fromEntityOrder(person);
     }
     public void deleteOrder(int id, int personId){
-        Session session = EMUtil.getSession();
-        session.beginTransaction();
+        Session session = sessionFactory.getCurrentSession();//EMUtil.getSession();
+//        session.beginTransaction();
 //        PersonEntity personEntity = session.get(PersonEntity.class, personId);
 //        for (OrderEntity em: personEntity.getOrderEntities()){
 //            if(em.getId() == id){
@@ -72,8 +74,8 @@ public class DefaultOrderDAO implements OrderDAO {
         OrderEntity orderEntity = session.get(OrderEntity.class, id);
         session.remove(orderEntity);
       //  PersonEntity personEntity = session.get(PersonEntity.class, personId);
-       session.getTransaction().commit();
-        session.close();
+//       session.getTransaction().commit();
+//        session.close();
        // return PersonConverter.fromEntityOrderList(personEntity);
     }
 }
