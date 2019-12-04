332dc2b706673dbec77b9123f0c23620e4085a95
Vitalij <Shevstrukk@mail.ru>
2019-12-04 23:40:04 +0300

    add //

--- web/src/main/java/com/github/Shevstrukk/web/servlets/order/GetOrderList.java
+++ web/src/main/java/com/github/Shevstrukk/web/servlets/order/GetOrderList.java
@@ -5,6 +5,7 @@ import com.github.Shevstrukk.dao.entity.PersonEntity;
 import com.github.Shevstrukk.model.AuthUser;
 import com.github.Shevstrukk.model.Person;
 import com.github.Shevstrukk.service.orderService.DefaultOrderService;
+import com.github.Shevstrukk.service.orderService.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,17 +25,17 @@ import java.io.IOException;
 @RequestMapping
 public class GetOrderList  {
     @Autowired
-    DefaultOrderService defaultOrderService;
-    @GetMapping("/getOrderList")
-    public String doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        AuthUser authUser = (AuthUser)session.getAttribute("authUser");
-        Person person = authUser.getPerson();
-        int id = person.getId();
-        Person personList = defaultOrderService.getOrderList(id);
-        req.setAttribute("personList", personList);
-        return "/order/orderList";
-//        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/order/orderList.jsp");
-//        requestDispatcher.forward(req, resp);
-    }
+    OrderService defaultOrderService;
+//    @GetMapping("/getOrderList")
+//    public String doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        HttpSession session = req.getSession();
+//        AuthUser authUser = (AuthUser)session.getAttribute("authUser");
+//        Person person = authUser.getPerson();
+//        int id = person.getId();
+//        Person personList = defaultOrderService.getOrderList(id);
+//        req.setAttribute("personList", personList);
+//        return "/order/orderList";
+////        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/order/orderList.jsp");
+////        requestDispatcher.forward(req, resp);
+//    }
 }
