361ad38d935ba05fe3f8e8206ba2483803c1b646
Vitalij <Shevstrukk@mail.ru>
2019-12-04 23:36:10 +0300

    add Transactional

--- service/src/main/java/com/github/Shevstrukk/service/phoneservice/DefaultPhoneService.java
+++ service/src/main/java/com/github/Shevstrukk/service/phoneservice/DefaultPhoneService.java
@@ -10,17 +10,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Service
+
 public class DefaultPhoneService implements PhoneService {
     private static final Logger log = LoggerFactory.getLogger(DefaultPhoneService.class);
     @Autowired
-    DefaultPhoneDAO defaultPhoneDAO;
+    PhoneDAO defaultPhoneDAO;
+    public DefaultPhoneService(PhoneDAO defaultPhoneDAO){this.defaultPhoneDAO = defaultPhoneDAO;}
 
+    @Transactional
     public Person savePhone(Phone phoneEntity, int id){
         return  defaultPhoneDAO.savePhone(phoneEntity, id);
     }
-
+    @Transactional
     public Person deletePhone( int id){
         return   defaultPhoneDAO.deletePhone(id);
     }
