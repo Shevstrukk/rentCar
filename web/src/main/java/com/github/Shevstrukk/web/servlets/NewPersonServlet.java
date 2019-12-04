1a15b2949965a61f8e93f3e0757ef1dad114e262
Vitalij <Shevstrukk@mail.ru>
2019-12-04 23:44:15 +0300

    add

--- web/src/main/java/com/github/Shevstrukk/web/servlets/NewPersonServlet.java
+++ web/src/main/java/com/github/Shevstrukk/web/servlets/NewPersonServlet.java
@@ -8,6 +8,7 @@ import com.github.Shevstrukk.model.AuthUser;
 import com.github.Shevstrukk.model.Person;
 import com.github.Shevstrukk.service.DefaultPersonService;
 import com.github.Shevstrukk.service.DefaultUserService;
+import com.github.Shevstrukk.service.PersonService;
 import com.github.Shevstrukk.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,12 +26,12 @@ import java.io.IOException;
 
 // @WebServlet("/newPerson")
 @Controller
-@RequestMapping
+@RequestMapping()
 public class NewPersonServlet   {
     @Autowired
-    DefaultUserService defaultUserService;
+    UserService userService;
     @Autowired
-    DefaultPersonService defaultPersonService;
+    PersonService defaultPersonService;
     @GetMapping("/newPerson")
     public String doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         return "newPerson";
@@ -47,7 +48,7 @@ public class NewPersonServlet   {
         final int home = Integer.valueOf(req.getParameter("home"));
         final int number = Integer.valueOf(req.getParameter("number"));
         Address addressEntity = new Address(null,state, city, street, home,number, null);
-        AuthUser user = defaultUserService.login(firstName, lastName);
+        AuthUser user = userService.login(firstName, lastName);
         Person person = new Person(null,firstName,lastName, user, addressEntity,null,null);
         Person person1= defaultPersonService.insertPerson(person);
         req.getSession().setAttribute("person1", person1);
