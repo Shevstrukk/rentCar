b355d0cc29fdb712b6589171aa5148834ad966ee
Vitalij <Shevstrukk@mail.ru>
2019-12-04 23:32:08 +0300

    add SF

--- dao/src/main/java/com/github/Shevstrukk/dao/carDao/DefaultCarDAO.java
+++ dao/src/main/java/com/github/Shevstrukk/dao/carDao/DefaultCarDAO.java
@@ -6,37 +6,39 @@ import com.github.Shevstrukk.dao.entity.CarEntity;
 import com.github.Shevstrukk.dao.util.EMUtil;
 import com.github.Shevstrukk.model.Car;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-@Repository
+
 public class DefaultCarDAO implements CarsDAO{
 
     private static final Logger log = LoggerFactory.getLogger(DefaultCarDAO.class);
+    private final SessionFactory sessionFactory;
 
-    public DefaultCarDAO() {
+    public DefaultCarDAO(SessionFactory sessionFactory) { this.sessionFactory = sessionFactory;
     }
 
     @Override
     public List<Car> getListCar() {
         List<CarEntity> carEntityList;
-        Session session = EMUtil.getSession();
-        session.beginTransaction();
+        Session session = sessionFactory.getCurrentSession();//EMUtil.getSession();
+//        session.beginTransaction();
         String str = "FROM CarEntity  ORDER BY id ASC";
         carEntityList = session.createQuery(str).getResultList();
-        session.getTransaction().commit();
-        session.close();
+//        session.getTransaction().commit();
+//        session.close();
         return CarConverter.fromListEntityCar(carEntityList);
     }
     public Car getCar(int id){
         CarEntity carEntity;
-        Session session = EMUtil.getSession();
-        session.beginTransaction();
+        Session session = sessionFactory.getCurrentSession();// EMUtil.getSession();
+//        session.beginTransaction();
         carEntity = session.get(CarEntity.class, id);
-        session.getTransaction().commit();
-        session.close();
+//        session.getTransaction().commit();
+//        session.close();
         return CarConverter.fromEntity(carEntity);
     }
 }
