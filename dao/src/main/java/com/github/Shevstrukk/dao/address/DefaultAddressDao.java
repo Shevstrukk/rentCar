48106f53d25f89c1f7a0900d281cc01e8aa4a127
Vitalij <Shevstrukk@mail.ru>
2019-12-04 23:21:05 +0300

    add sessionFactory

--- dao/src/main/java/com/github/Shevstrukk/dao/address/DefaultAddressDao.java
+++ dao/src/main/java/com/github/Shevstrukk/dao/address/DefaultAddressDao.java
@@ -5,22 +5,27 @@ import com.github.Shevstrukk.dao.entity.AddressEntity;
 import com.github.Shevstrukk.dao.util.EMUtil;
 import com.github.Shevstrukk.model.Address;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public class DefaultAddressDao implements AddressDao {
     private static final Logger log = LoggerFactory.getLogger(DefaultAddressDao.class);
-    public DefaultAddressDao() {    }
+    private final SessionFactory sessionFactory;
+    @Autowired
+    public DefaultAddressDao(SessionFactory sessionFactory) {this.sessionFactory = sessionFactory;    }
 
     public Address saveAddress( Address address){
         AddressEntity addressEntity = AddressConverter.toEntity(address);
-        Session session = EMUtil.getSession();
-        session.beginTransaction();
+       // Session session = EMUtil.getSession();
+      // session.beginTransaction();
+       final Session session = sessionFactory.getCurrentSession();
         session.save(addressEntity);
-        session.getTransaction().commit();
-        session.close();
+//        session.getTransaction().commit();
+//        session.close();
         return AddressConverter.fromEntity(addressEntity);
     }
 
