118fec1580c537721cdc584bcb1b644ae037749b
Vitalij <Shevstrukk@mail.ru>
2019-12-04 23:21:51 +0300

    add Transactional

--- service/src/main/java/com/github/Shevstrukk/service/address/DefaultAddressService.java
+++ service/src/main/java/com/github/Shevstrukk/service/address/DefaultAddressService.java
@@ -1,23 +1,24 @@
 package com.github.Shevstrukk.service.address;
 
 import com.github.Shevstrukk.dao.address.AddressDao;
-import com.github.Shevstrukk.dao.address.DefaultAddressDao;
-import com.github.Shevstrukk.dao.base.BaseDao;
-import com.github.Shevstrukk.dao.entity.AddressEntity;
 import com.github.Shevstrukk.model.Address;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Service
-public class DefaultAddressService implements AdderessService{
+
+public class DefaultAddressService implements AddressService {
     private static final Logger log = LoggerFactory.getLogger(DefaultAddressService.class);
     @Autowired
     AddressDao defaultAddressDao;
 
-    public Address saveAddress(Address address){ return  defaultAddressDao.saveAddress(address);}
+    public DefaultAddressService(AddressDao address) { this.defaultAddressDao=address;}
 
+    @Override
+    @Transactional
+    public Address saveAddress(Address address) {
+        return  defaultAddressDao.saveAddress(address);
+    }
 }
 
