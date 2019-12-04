e50e6de4b2cb0d09149fdef9faa4971d676b9afe
Vitalij <Shevstrukk@mail.ru>
2019-12-04 23:32:32 +0300

    add

--- service/src/main/java/com/github/Shevstrukk/service/carService/DefaultCarsService.java
+++ service/src/main/java/com/github/Shevstrukk/service/carService/DefaultCarsService.java
@@ -1,21 +1,25 @@
 package com.github.Shevstrukk.service.carService;
 
+import com.github.Shevstrukk.dao.carDao.CarsDAO;
 import com.github.Shevstrukk.dao.carDao.DefaultCarDAO;
 import com.github.Shevstrukk.model.Car;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-@Service
+
 public class DefaultCarsService implements CarsService {
     private static final Logger log = LoggerFactory.getLogger(DefaultCarsService.class);
-    @Autowired
-    DefaultCarDAO defaultCarDAO;
 
-    public List<Car> getCars() {return defaultCarDAO.getListCar();   }
+   private  final CarsDAO carDAO;
 
-    public Car getCar(int id){return defaultCarDAO.getCar(id);}
+    public DefaultCarsService(CarsDAO carDAO){ this.carDAO= carDAO;}
+    @Override
+    @Transactional
+    public List<Car> getCars() {return carDAO.getListCar();   }
+    @Override
+    @Transactional
+    public Car getCar(int id){return carDAO.getCar(id);}
 }
