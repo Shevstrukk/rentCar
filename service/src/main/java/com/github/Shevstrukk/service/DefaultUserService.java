5c97fc7c7a42887096ba624c210aa5f7c620dd6e
Vitalij <Shevstrukk@mail.ru>
2019-12-04 23:36:40 +0300

    add Trans

--- service/src/main/java/com/github/Shevstrukk/service/DefaultUserService.java
+++ service/src/main/java/com/github/Shevstrukk/service/DefaultUserService.java
@@ -6,14 +6,19 @@ import com.github.Shevstrukk.dao.entity.AuthUserEntity;
 import com.github.Shevstrukk.model.AuthUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.List;
-@Service
-public class DefaultUserService implements UserService {
-    @Autowired
-    DefaultAuthUsersDAO defaultAuthUsersDAO;
 
+public class DefaultUserService implements UserService {
+
+    @Autowired
+    private final AuthUsersDAO authUsersDAO;
+    public DefaultUserService(AuthUsersDAO authUsersDAO){
+        this.authUsersDAO = authUsersDAO;
+    }
+    @Transactional
     public AuthUser login(String login, String password) {
         List<AuthUser> userList = listAllUsers();
        // AuthUser user;
@@ -28,20 +33,22 @@ public class DefaultUserService implements UserService {
         }
         //return user = addAuthUser(login,password);
     }
+    @Transactional
     public AuthUser addAuthUser(String login, String password){
-        AuthUser newAuthUser =   defaultAuthUsersDAO.saveOrUpdateAuthUser(new AuthUser(null,login,password,"user",null));
+        AuthUser newAuthUser =   authUsersDAO.saveOrUpdateAuthUser(new AuthUser(null,login,password,"user",null));
         return newAuthUser;
 
     }
+    @Transactional
     public List<AuthUser> listAllUsers () {
-        return defaultAuthUsersDAO.listAllUsers();
+        return authUsersDAO.listAllUsers();
     }
-
+    @Transactional
     public void deleteAuthUser (int id){
-        defaultAuthUsersDAO.deleteAuthUser( id);
+        authUsersDAO.deleteAuthUser( id);
     }
-
-    public AuthUser update(int id, int personId){ return defaultAuthUsersDAO.update(id, personId);}
+    @Transactional
+    public AuthUser update(int id, int personId){ return authUsersDAO.update(id, personId);}
 
 
 }
