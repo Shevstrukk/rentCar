9750be7d94955f0c270d83e7103c68c248949e53
Vitalij <Shevstrukk@mail.ru>
2019-12-04 23:40:29 +0300

    add //

--- web/src/main/java/com/github/Shevstrukk/web/servlets/order/GetOrderServlet.java
+++ web/src/main/java/com/github/Shevstrukk/web/servlets/order/GetOrderServlet.java
@@ -2,6 +2,7 @@ package com.github.Shevstrukk.web.servlets.order;
 
 import com.github.Shevstrukk.dao.entity.CarEntity;
 import com.github.Shevstrukk.model.Car;
+import com.github.Shevstrukk.service.carService.CarsService;
 import com.github.Shevstrukk.service.carService.DefaultCarsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,21 +21,21 @@ import java.io.IOException;
 @Controller
 @RequestMapping
 public class GetOrderServlet  {
-    @Autowired
-    DefaultCarsService defaultCarsService;
-
-    @PostMapping("/getOrder")
-    public String doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final int id = Integer.parseInt(req.getParameter("id"));
-        final int rentDay = Integer.parseInt(req.getParameter("rentDay"));
-
-        Car carEntity = defaultCarsService.getCar(id);
-        int price = rentDay* carEntity.getPriceDay();
-        req.setAttribute("carEntity", carEntity);
-        req.setAttribute("price", price);
-        req.setAttribute("rentDay", rentDay);
-        return "/order/addOrder";
-//        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/order/addOrder.jsp");
-//        requestDispatcher.forward(req, resp);
-    }
+//    @Autowired
+//    CarsService defaultCarsService;
+//
+//    @PostMapping("/getOrder")
+//    public String doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        final int id = Integer.parseInt(req.getParameter("id"));
+//        final int rentDay = Integer.parseInt(req.getParameter("rentDay"));
+//
+//        Car carEntity = defaultCarsService.getCar(id);
+//        int price = rentDay* carEntity.getPriceDay();
+//        req.setAttribute("carEntity", carEntity);
+//        req.setAttribute("price", price);
+//        req.setAttribute("rentDay", rentDay);
+//        return "/order/addOrder";
+////        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/order/addOrder.jsp");
+////        requestDispatcher.forward(req, resp);
+//    }
 }
