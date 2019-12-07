7db9255ac7057271913392dee3a0b3529c58b8b2
Vitalij <Shevstrukk@mail.ru>
2019-12-07 12:49:14 +0300

    add sendRedirect

--- web/src/main/java/com/github/Shevstrukk/web/servlets/LogoutController.java
+++ web/src/main/java/com/github/Shevstrukk/web/servlets/LogoutController.java
@@ -1,5 +1,8 @@
 package com.github.Shevstrukk.web.servlets;
 
+import com.github.Shevstrukk.service.ConfigService;
+import com.github.Shevstrukk.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +15,12 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 @Controller
 @RequestMapping
-public class LogoutServlet  {
+public class LogoutController  {
+    private final UserService userService;
+    @Autowired
+    public LogoutController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/logout")
     protected String doGet(HttpServletRequest req)  {
