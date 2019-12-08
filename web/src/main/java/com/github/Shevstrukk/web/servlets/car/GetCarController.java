0fda1223f5920273bc548375cfed656a498a9cdb
Vitalij <Shevstrukk@mail.ru>
2019-12-08 15:09:10 +0300

    add model

--- web/src/main/java/com/github/Shevstrukk/web/servlets/car/GetCarController.java
+++ web/src/main/java/com/github/Shevstrukk/web/servlets/car/GetCarController.java
@@ -5,6 +5,7 @@ import com.github.Shevstrukk.model.Car;
 import com.github.Shevstrukk.service.carService.CarsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,10 +17,11 @@ import java.util.List;
 public class GetCarController {
         @Autowired
         CarsService defaultCarsService;
-        @GetMapping("/getCars")
-        protected String doGet(HttpServletRequest req) {
+        @GetMapping("/getCarsAuth")
+        protected String doGet(HttpServletRequest req, Model model) {
             List<Car> carList = defaultCarsService.getCars();
-            req.setAttribute("carEntityList", carList);
+            model.addAttribute("carEntityList", carList);
+           // req.setAttribute("carEntityList", carList);
             return "/car/carList";
 //            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/car/carList.jsp");
 //            requestDispatcher.forward(req, resp);
